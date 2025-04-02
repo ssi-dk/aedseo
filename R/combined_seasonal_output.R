9de053e9c9f2c74daea44a244eae1ef1ee09f1b1
SofiaOtero <myrup.otero@gmail.com>
2025-04-02 17:34:39 +0200

    Adding multiple waves

--- R/combined_seasonal_output.R
+++ R/combined_seasonal_output.R
@@ -11,6 +11,13 @@
 #' @param disease_threshold `r rd_disease_threshold(usage = "combined")`
 #' @param family `r rd_family(usage = "combined")`
 #' @param family_quant A character string specifying the family for modeling burden levels.
+#' @param multiple_waves A logical. Should the output contain multiple seasonal onsets?
+#' @param burden_level_decrease A character specifying the burden breakpoint the observations should decrease under
+#' before a new increase in observations can call a new seasonal onset if seasonal onset criteria are met.
+#' Choose between; "very low", "low", "medium", or "high".
+#' @param steps_with_decrease An integer specifying in how many time steps (days, weeks, months) the decrease
+#' should be observed (if there is a sudden decrease followed by an increase it could e.g. be due to testing).
+#' If multiple_waves are assigned steps_with_decrease defaults to 1.
 #' @param ... Arguments passed to `seasonal_burden_levels()`, `fit_percentiles()` and `seasonal_onset()` functions.
 #'
 #' @return An object containing two lists: onset_output and burden_output:
@@ -61,7 +68,7 @@
 #' print(combined_data$onset_output)
 #' # Print burden level results
 #' print(combined_data$burden_output)
-combined_seasonal_output <- function(
+combined_seasonal_output <- function(         # nolint: cyclocomp_linter.
   tsd,
   disease_threshold = 20,
   family = c(
@@ -76,8 +83,33 @@ combined_seasonal_output <- function(
   season_start = 21,
   season_end = season_start - 1,
   only_current_season = TRUE,
+  multiple_waves = FALSE,
+  burden_level_decrease = NULL,
+  steps_with_decrease = NULL,
   ...
 ) {
+  coll <- checkmate::makeAssertCollection()
+  checkmate::assert_logical(multiple_waves, add = coll)
+  checkmate::assert_character(burden_level_decrease, null.ok = TRUE, add = coll)
+  checkmate::assert_choice(burden_level_decrease,
+                           choices = c("very low", "low", "medium", "high"),
+                           null.ok = TRUE,
+                           add = coll)
+  checkmate::assert_integerish(steps_with_decrease, lower = 1, null.ok = TRUE, add = coll)
+  if (!multiple_waves && !is.null(burden_level_decrease)) {
+    coll$push("burden_level_decrease is not used unless multiple_waves is TRUE.")
+  }
+  if (!multiple_waves && !is.null(steps_with_decrease)) {
+    coll$push("steps_with_decrease is not used unless multiple_waves is TRUE.")
+  }
+  if (multiple_waves && is.null(burden_level_decrease)) {
+    coll$push("burden_level_decrease must be assigned if multiple_waves is TRUE")
+  }
+  if (multiple_waves && is.null(steps_with_decrease)) {
+    warning("steps_with_decrease is by default set to 1")
+    steps_with_decrease <- 1
+  }
+  checkmate::reportAssertions(coll)
 
   # Capture all extra arguments
   extra_args <- list(...)
@@ -104,6 +136,60 @@ combined_seasonal_output <- function(
            season_start = season_start, season_end = season_end, only_current_season = only_current_season),
       onset_args)
   )   # nolint: object_usage_linter.
+
+  # Add multiple waves if assigned in input
+  if (multiple_waves && isTRUE(only_current_season)) {
+    decrease_below <- burden_output$values[[burden_level_decrease]]
+
+    # Add new columns for wave_number, wave_starts and decrease_counter
+    onset_output <- onset_output |>
+      dplyr::mutate(
+        wave_number = NA_integer_,
+        wave_starts = FALSE,
+        decrease_counter = NA_integer_
+      )
+
+    in_wave <- FALSE      # Are we currently in a wave?
+    wave_counter <- 0     # Wave counter
+    decrease_counter <- 0 # Counter for consecutive steps where decrease is observed
+
+    # Iterate over onset_output
+    for (i in seq_len(nrow(onset_output))) {
+      if (!in_wave) {
+        # Not currently in a wave, look for a wave start signal:
+        if (isTRUE(onset_output$seasonal_onset_alarm[i])) {
+          wave_counter <- wave_counter + 1
+          in_wave <- TRUE
+          onset_output$wave_number[i] <- wave_counter
+          onset_output$wave_starts[i] <- TRUE  # Mark the beginning of a wave
+          decrease_counter <- 0  # Reset the decrease counter at wave start
+          onset_output$decrease_counter[i] <- decrease_counter
+        }
+      } else {
+        # If already in a wave, assign the current wave number to this row
+        onset_output$wave_number[i] <- wave_counter
+
+        # Check if the current observation is decreasing compared to the previous row
+        # and falls below the decrease_below threshold.
+        if (onset_output$observation[i] < onset_output$observation[i - 1] &&
+              onset_output$observation[i] < decrease_below) {
+          decrease_counter <- decrease_counter + 1
+          onset_output$decrease_counter[i] <- decrease_counter
+        } else {
+          # Reset the counter if decrease does not persist
+          decrease_counter <- 0
+          onset_output$decrease_counter[i] <- decrease_counter
+        }
+
+        # If the number of consecutive decreasing steps reaches the specified threshold end the current wave.
+        if (decrease_counter == steps_with_decrease) {
+          in_wave <- FALSE
+          onset_output$decrease_counter[i] <- decrease_counter
+          decrease_counter <- 0  # Reset counter for next wave detection
+        }
+      }
+    }
+  }
 
   # Combine both results in lists
   seasonal_output <- list(
