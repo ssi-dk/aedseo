5ff6bd1f3e1ca94112c015bf684dcee599ca2796
Lasse Engbo Christiansen <lsec@ssi.dk>
2026-07-21 15:51:34 +0200

    feat(fit_growth_rate): Made a more generic version using observations and denominator as inputs and choosing formula based on the chosen family.

--- R/fit_growth_rate.R
+++ R/fit_growth_rate.R
@@ -3,15 +3,10 @@
 #' @description
 #'
 #' This function fits a growth rate model to time series observations and provides parameter estimates along with
-#' confidence intervals. For binomial data, supply successes and trials and use `family = "binomial"` or
-#' `family = "quasibinomial"`.
+#' confidence intervals. For binomial data use `family = "binomial"` or `family = "quasibinomial"`.
 #'
-#' @param cases `r rd_cases`
-#' @param successes An integer vector containing binomial successes. This is an alias for `cases` for
-#' binomial/quasibinomial models.
-#' @param population `r rd_population`
-#' @param trials An integer vector containing binomial trials. This is an alias for `population` for
-#' binomial/quasibinomial models.
+#' @param observations An integer vector containing cases or binomial successes.
+#' @param denominator An integer vector containing population or binomial trials. This is mandatory for binomial models
 #' @param level The confidence level for parameter estimates, a numeric value between 0 and 1.
 #' @param family `r rd_family()`
 #'
@@ -28,22 +23,20 @@
 #' # (e.g., population growth)
 #' data <- c(100, 120, 150, 180, 220, 270)
 #' fit_growth_rate(
-#'   cases = data,
+#'   observations = data,
 #'   level = 0.95,
 #'   family = "poisson"
 #' )
 #'
 #' # Fit a binomial growth rate model to successes out of trials
 #' fit_growth_rate(
-#'   successes = c(1, 2, 3, 4),
-#'   trials = c(10, 10, 10, 10),
+#'   observations = c(1, 2, 3, 4),
+#'   denominator = c(10, 10, 10, 10),
 #'   family = "binomial"
 #' )
 fit_growth_rate <- function(
-  cases = NULL,
-  successes = NULL,
-  population = NULL,
-  trials = NULL,
+  observations = NULL,
+  denominator = NULL,
   level = 0.95,
   family = c(
     "quasipoisson",
@@ -56,21 +49,9 @@ fit_growth_rate <- function(
 
   # Check input arguments
   coll <- checkmate::makeAssertCollection()
-  if (!is.null(successes)) {
-    if (!is.null(cases)) {
-      coll$push("Supply only one of `cases` or `successes`")
-    }
-    cases <- successes
-  }
-  if (!is.null(trials)) {
-    if (!is.null(population)) {
-      coll$push("Supply only one of `population` or `trials`")
-    }
-    population <- trials
-  }
-  checkmate::assert_numeric(cases, null.ok = FALSE, add = coll)
+  checkmate::assert_numeric(observations, null.ok = FALSE, add = coll)
   checkmate::assert_numeric(level, lower = 0, upper = 1, add = coll)
-  checkmate::assert_numeric(population, null.ok = TRUE, add = coll)
+  checkmate::assert_numeric(denominator, null.ok = TRUE, add = coll)
   # Match the selected model
   if (is.character(family)) { # If character
     fam_name <- match.arg(family)
@@ -90,40 +71,35 @@ fit_growth_rate <- function(
     choices = c("poisson", "quasipoisson", "binomial", "quasibinomial"),
     add = coll
   )
-  if (fam_obj$family %in% c("binomial", "quasibinomial")) {
-    if (is.null(population)) {
-      coll$push("`trials` (or `population`) must be supplied for binomial and quasibinomial models")
+  is_binomial_type <- fam_obj$family %in% c("binomial", "quasibinomial")
+  if (is_binomial_type) {
+    if (is.null(denominator)) {
+      coll$push("`denominator` must be supplied for binomial and quasibinomial models")
     } else {
-      checkmate::assert_true(all(cases >= 0, na.rm = TRUE), add = coll)
-      checkmate::assert_true(all(population > 0, na.rm = TRUE), add = coll)
-      checkmate::assert_true(all(cases <= population, na.rm = TRUE), add = coll)
+      checkmate::assert_true(all(observations >= 0, na.rm = TRUE), add = coll)
+      checkmate::assert_true(all(denominator > 0, na.rm = TRUE), add = coll)
+      checkmate::assert_true(all(observations <= denominator, na.rm = TRUE), add = coll)
     }
   }
   checkmate::reportAssertions(coll)
 
   # Construct the data with growth rates for the glm model
   growth_data <- purrr::compact(list(
-    growth_rate = seq_along(cases),
-    cases = cases,
-    population = population,
-    successes = cases,
-    trials = population
+    growth_rate = seq_along(observations),
+    observations = observations,
+    denominator = denominator,
   )) |>
     tibble::as_tibble()
 
   # Construct formula terms
-  terms <- if (is.null(population)) {
+  terms <- if (is.null(denominator)) {
     "growth_rate"
-  } else if (fam_obj$family %in% c("binomial", "quasibinomial")) {
+  } else if (is_binomial_type) {
     "growth_rate"
   } else {
-    c("growth_rate", "offset(log(population))")
+    c("growth_rate", "offset(log(denominator))")
   }
-  response <- if (fam_obj$family %in% c("binomial", "quasibinomial")) {
-    "cbind(successes, trials - successes)"
-  } else {
-    "cases"
-  }
+  response <- ifelse(is_binomial_type, "cbind(observations, denominator - observations)", "observations")
 
   # Fit the model
   growth_fit <- stats::glm(
