10d8d0b09717c7fa5b7adab040fe36561b724218
Lasse Engbo Christiansen <lsec@ssi.dk>
2026-07-24 14:02:12 +0200

    fix: Typo

--- R/seasonal_onset.R
+++ R/seasonal_onset.R
@@ -91,9 +91,9 @@ seasonal_onset <- function(
     family_char <- family
   } else if(inherits(family, "family")) {
     family_char <- family$family
-  } else if(inherits(family, "function")) {
+  } else if(is(family, "function")) {
     tmp <- eval(as.call(list(family)))
-    if(inherits(family, "family")) {
+    if(inherits(tmp, "family")) {
       family_char <- tmp$family
     } else {
       coll$push(
@@ -101,6 +101,7 @@ seasonal_onset <- function(
       )
     }
   }
+  checkmate::reportAssertions(coll)
 
   # Deciding which model outcome to use
   incidence_denominator <- attr(tsd, "incidence_denominator")
